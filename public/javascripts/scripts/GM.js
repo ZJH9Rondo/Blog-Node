d8c4446c950883b203f07930f943c0c7a28bf00a
ZJH9Rondo <779247142@qq.com>
2017-08-07 17:27:34 +0800

    add:navigator detection tips for textarea paste events

--- public/javascripts/scripts/GM.js
+++ public/javascripts/scripts/GM.js
@@ -238,6 +238,84 @@ define(function(){
                   new Ajax(params);
                 }
               };
+          },
+      // 识别浏览器
+      navigator_Agent: function (){
+        var engine = {
+          // 呈现引擎
+          ie: 0,
+          gecko: 0,
+          webkit: 0,
+          khtml: 0,
+          opera: 0,
+
+          version: null // 具体版本
+        };
+
+        var browser = {
+          // 浏览器
+          ie: 0,
+          firefox: 0,
+          safari: 0,
+          konp: 0,
+          opera: 0,
+          chrome: 0,
+
+          version: null // 具体版本
+        };
+
+        // 检测呈现引擎和浏览器
+        var ua = navigator.userAgent;
+        if(window.opera){
+          engine.ver = browser.ver = window.opera.versiom();
+          engine.opera = browser.opera = parseFloat(engine.ver);
+        }else if(/AppleWebKit\/(\S+)/.test(ua)){
+          engine.ver = RegExp["$1"];
+          engine.webkit = parseFloat(engine.ver);
+
+          // 确定是Chrome 还是 Safari
+          if(/Chrome\/(\S+)/.test(ua)){
+            browser.ver = RegExp["$1"];
+            browser.chrome = parseFloat(browser.ver);
+          }else if(/Version\/(\S+)/.test(ua)){
+            browser.ver = RegExp["$1"];
+            browser.safari = parseFloat(browser.ver);
+          }else{
+            // 近似确定版本号
+            var safariVersion = 1;
+            if(engine.webkit < 100){
+              safariVersion = 1;
+            }else if(engine.webkit < 312){
+              safariVersion = 1.2;
+            }else if(engine.webkit < 412){
+              safariVersion = 1.3;
+            }else{
+              safariVersion = 2;
+            }
+
+            browser.safari = browser.ver = safariVersion;
           }
-      };
+        }else if(/KHTML\/(\S+)/.test(ua) || /Konqueror\/([^;]+)/.test(ua)){
+          engine.ver = browser.ver = RegExp["$1"];
+          engine.khtml = browser.konq = parseFloat(engine.ver);
+        }else if(/rv:([^\)]+)\) Gecko\/\d{8}/.test(ua)){
+          engine.ver = RegExp["$1"];
+          engine.gecko = parseFloat(engine.ver);
+
+          // 判断firefox
+          if(/Firefox\/(\S+)/.test(ua)){
+            browser.ver = RegExp["$1"];
+            browser.firefox = parseFloat(browser.ver);
+          }
+        }else if(/MSIE ([^;]+)/.test(ua)){
+          engine.ver = browser.ver = RegExp["$1"];
+          engine.ie = browser.ie = parseFloat(engine.ver);
+        }
+
+        return {
+          engine: engine,
+          browser: browser
+        };
+      }
+    };
 });
--- public/javascripts/scripts/upload.js
+++ public/javascripts/scripts/upload.js
@@ -7,14 +7,66 @@ define(['GM'],function (GM){
   if(document.getElementById('createform')){
     (function (){
         var createform = document.getElementById('createform'),
+            tips = document.getElementById('tips'),
             Ajax = GM.ajax(),
+            client = GM.navigator_Agent(),
             imageFile;
 
+        // 检测浏览器
+        // create tips
+        (function (){
+            function navigator_supply(){
+              var text = tips.childNodes[0];
+
+              text.innerText = '当前浏览器，文本编辑插入图片同时支持鼠标及键盘复制粘贴上传';
+              tips.style.display = 'block';
+              tips.style.background = 'rgba(166,244,142,0.5)';
+              text.style.color = '#359924';
+            }
+
+            function navigator_off(tips_text){
+              var text = tips.childNodes[0];
+
+              text.innerText = tips_text;
+              tips.style.display = 'block';
+              tips.style.background = 'rgba(251,215,210,1)';
+              text.style.color = '#C63939';
+            }
+
+            function remove_Tips(){
+              tips.addEventListener("click",function (){
+                this.style.display = 'none';
+              },false);
+            }
+
+            if(client.engine.webkit){
+              if(client.browser.chrome){
+                navigator_supply();
+                remove_Tips();
+              }else if(client.browser.safari){
+                navigator_supply();
+                remove_Tips();
+              }
+            }else if(client.engine.gecko){
+              if(client.browser.firefox){
+                var tips_firefox = '当前浏览器，文本编辑插入图片仅支持Ctrl+V复制粘贴上传';
+
+                navigator_off(tips_firefox);
+                remove_Tips();
+              }else{
+                var tips_others = '当前浏览器，文本编辑复制粘贴插入图片功能不保证完全支持，抱歉。';
+                navigator_off(tips_others);
+                remove_Tips();
+              }
+            }
+        })();
+
+        // textarea paste event
         createform.addEventListener('paste',function (e){
           // only for chrome
           e = e || event;
           e.preventDefault();
-          var clipboardData = e.clipboardData; // clipboardData 获取事件数据
+          var clipboardData = e.clipboardData || window.clipboardData; // clipboardData chrome,firefox || IE
 
           for(var i = 0,len = clipboardData.items.length;i < len; i++){
             if(clipboardData.items[i].kind == 'file' || clipboardData.items[i].type.indexOf('image') > -1){
@@ -28,10 +80,10 @@ define(['GM'],function (GM){
               // event.target.result 即为图片的Base64编码字符串
               var base64_str = event.target.result,
                   upload_url,
-                  upToken;
+                  UpToken;
 
               // 客户端直传函数
-              function upload_base64(base64_str,upload_url,token){
+              function upload_base64(base64_str,upload_url,UpToken){
                 var pic = base64_str,
                     url = upload_url,
                     xhr = new XMLHttpRequest();
@@ -39,14 +91,17 @@ define(['GM'],function (GM){
                 xhr.onreadystatechange = function (){
                   if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200){
                       var img = JSON.parse(xhr.responseText),
-                          Imgsrc = 'http://ou6y87mzi.bkt.clouddn.com/' + img.key;
+                          //Imgsrc = 'http://ou6y87mzi.bkt.clouddn.com/' + img.key;
+                          // 测试链接
+                          Imgsrc = 'http://7xqqm5.com1.z0.glb.clouddn.com/' + img.key;
 
+                      console.log(img);
                       markdown_IMG(Imgsrc);
                   }
                 };
                 xhr.open("POST",url,true);
                 xhr.setRequestHeader("Content-Type", "application/octet-stream");
-                xhr.setRequestHeader("Authorization",'UpToken' + ' ' + token);
+                xhr.setRequestHeader("Authorization",'UpToken' + ' ' + UpToken);
                 xhr.send(pic);
              }
 
@@ -70,11 +125,12 @@ define(['GM'],function (GM){
               success: function (result){
 
               base64_str = base64_str.replace(/^data:image\/\w+;base64,/, '');
-              upload_url = 'http://upload-z2.qiniu.com/putb64/' + imageFile.size;
+              // 测试链接
+              upload_url = 'http://upload.qiniu.com/putb64/' + imageFile.size;
               result = JSON.parse(result);
-              upToken = result.token;
+              UpToken = result.token;
               // 上传文件
-              upload_base64(base64_str,upload_url,upToken);
+              upload_base64(base64_str,upload_url,UpToken);
             }
           });
         };
