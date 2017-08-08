0b2b306de3146fc5abfd5b8995fceae7f0467b1e
ZJH9Rondo <779247142@qq.com>
2017-08-08 16:03:44 +0800

    add:drag img to textarea upload

--- public/javascripts/scripts/upload.js
+++ public/javascripts/scripts/upload.js
@@ -12,33 +12,33 @@ define(['GM'],function (GM){
             client = GM.navigator_Agent(),
             imageFile;
 
-        // 检测浏览器
         // create tips
+        function navigator_supply(){
+          var text = tips.childNodes[0];
+
+          text.innerText = '当前浏览器，文本编辑插入图片同时支持鼠标及键盘复制粘贴上传';
+          tips.style.display = 'block';
+          tips.style.background = 'rgba(166,244,142,0.5)';
+          text.style.color = '#359924';
+        }
+
+        function navigator_off(tips_text){
+          var text = tips.childNodes[0];
+
+          text.innerText = tips_text;
+          tips.style.display = 'block';
+          tips.style.background = 'rgba(251,215,210,1)';
+          text.style.color = '#C63939';
+        }
+
+        function remove_Tips(){
+          tips.addEventListener("click",function (){
+            this.style.display = 'none';
+          },false);
+        }
+
+        // client engine check
         (function (){
-            function navigator_supply(){
-              var text = tips.childNodes[0];
-
-              text.innerText = '当前浏览器，文本编辑插入图片同时支持鼠标及键盘复制粘贴上传';
-              tips.style.display = 'block';
-              tips.style.background = 'rgba(166,244,142,0.5)';
-              text.style.color = '#359924';
-            }
-
-            function navigator_off(tips_text){
-              var text = tips.childNodes[0];
-
-              text.innerText = tips_text;
-              tips.style.display = 'block';
-              tips.style.background = 'rgba(251,215,210,1)';
-              text.style.color = '#C63939';
-            }
-
-            function remove_Tips(){
-              tips.addEventListener("click",function (){
-                this.style.display = 'none';
-              },false);
-            }
-
             if(client.engine.webkit){
               if(client.browser.chrome){
                 navigator_supply();
@@ -61,80 +61,129 @@ define(['GM'],function (GM){
             }
         })();
 
-        // textarea paste event
-        createform.addEventListener('paste',function (e){
-          // only for chrome
-          e = e || event;
-          e.preventDefault();
-          var clipboardData = e.clipboardData || window.clipboardData; // clipboardData chrome,firefox || IE
+    // 客户端直传函数
+    function upload_base64(base64_str,upload_url,UpToken){
+          var pic = base64_str,
+              url = upload_url,
+              xhr = new XMLHttpRequest();
 
-          for(var i = 0,len = clipboardData.items.length;i < len; i++){
-            if(clipboardData.items[i].kind == 'file' || clipboardData.items[i].type.indexOf('image') > -1){
-              imageFile = clipboardData.items[i].getAsFile();
+          xhr.onreadystatechange = function (){
+            if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200){
+                var img = JSON.parse(xhr.responseText),
+                    //Imgsrc = 'http://ou6y87mzi.bkt.clouddn.com/' + img.key;
+                    // 测试链接
+                    Imgsrc= 'http://7xqqm5.com1.z0.glb.clouddn.com/' + img.key;
+
+                markdown_IMG(Imgsrc);
             }
-          }
+          };
+          xhr.open("POST",url,true);
+          xhr.setRequestHeader("Content-Type", "application/octet-stream");
+          xhr.setRequestHeader("Authorization",'UpToken' + ' ' + UpToken);
+          xhr.send(pic);
+    }
 
-          var reader = new FileReader();
-          reader.readAsDataURL(imageFile);
-          reader.onload = function (event) {
-              // event.target.result 即为图片的Base64编码字符串
-              var base64_str = event.target.result,
-                  upload_url,
-                  UpToken;
+    // Markdown语法插入图片
+    function markdown_IMG(Imgsrc){
+      var selectionStart = createform.selectionStart,
+          selectionEnd = createform.selectionEnd,
+          selectionText = createform.value,
+          insert_Img;
 
-              // 客户端直传函数
-              function upload_base64(base64_str,upload_url,UpToken){
-                var pic = base64_str,
-                    url = upload_url,
-                    xhr = new XMLHttpRequest();
+      insert_Img = '![MyBlog](' + Imgsrc +')'; // markdown 语法插入图片
+      createform.value = selectionText.substring(0,selectionStart) + insert_Img + selectionText.substring(selectionEnd,selectionText.length);
+      selectionStart = selectionEnd = selectionStart + insert_Img.length; //移动光标
+    }
 
-                xhr.onreadystatechange = function (){
-                  if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200){
-                      var img = JSON.parse(xhr.responseText),
-                          //Imgsrc = 'http://ou6y87mzi.bkt.clouddn.com/' + img.key;
-                          // 测试链接
-                          Imgsrc = 'http://7xqqm5.com1.z0.glb.clouddn.com/' + img.key;
 
-                      console.log(img);
-                      markdown_IMG(Imgsrc);
-                  }
-                };
-                xhr.open("POST",url,true);
-                xhr.setRequestHeader("Content-Type", "application/octet-stream");
-                xhr.setRequestHeader("Authorization",'UpToken' + ' ' + UpToken);
-                xhr.send(pic);
-             }
+    function getToken(img_origin) {
+        var reader = new FileReader();
+        // event.target.result === Img Base64_code
+        function upload_Item(img_origin,token){
+          reader.readAsDataURL(img_origin);
 
-             // Markdown语法插入图片
-             function markdown_IMG(Imgsrc){
-               var selectionStart = createform.selectionStart,
-                   selectionEnd = createform.selectionEnd,
-                   selectionText = createform.value,
-                   insert_Img;
+          reader.onload = function (event){
+            var base64_str = event.target.result,
+                upload_url,
+                UpToken;
 
-               insert_Img = '![MyBlog](' + Imgsrc +')'; // markdown 语法插入图片
-               createform.value = selectionText.substring(0,selectionStart) + insert_Img + selectionText.substring(selectionEnd,selectionText.length);
-               selectionStart = selectionEnd = selectionStart + insert_Img.length; //移动光标
-             }
+            base64_str = base64_str.replace(/^data:image\/\w+;base64,/, '');
+            // 测试链接
+            upload_url = 'http://upload.qiniu.com/putb64/' + img_origin.size;
+            UpToken = token;
+            // upload Image
+            upload_base64(base64_str,upload_url,UpToken);
+         };
+        }
 
-            // 获取token
-            Ajax.init({
-              url: '/getToken',
-              method: 'get',
-              datatype: 'json',
-              success: function (result){
+        Ajax.init({
+           url: '/getToken',
+           method: 'get',
+           datatype: 'json',
+           success: function (result){
 
-              base64_str = base64_str.replace(/^data:image\/\w+;base64,/, '');
-              // 测试链接
-              upload_url = 'http://upload.qiniu.com/putb64/' + imageFile.size;
-              result = JSON.parse(result);
-              UpToken = result.token;
-              // 上传文件
-              upload_base64(base64_str,upload_url,UpToken);
-            }
-          });
-        };
-      },false);
-    })();
+           result = JSON.parse(result);
+           // 上传文件
+           upload_Item(img_origin,result.token);
+         }
+       });
+     }
+    /*
+    * addEventListener
+    */
+    // textarea paste event
+    createform.addEventListener('paste',function (e){
+      // only for chrome
+      e = e || event;
+      e.preventDefault();
+      var clipboardData = e.clipboardData || window.clipboardData; // clipboardData chrome,firefox || IE
+
+      for(var i = 0,len = clipboardData.items.length;i < len; i++){
+        if(clipboardData.items[i].kind == 'file' || clipboardData.items[i].type.indexOf('image') > -1){
+          imageFile = clipboardData.items[i].getAsFile();
+        }
+      }
+
+      // getToken and upload base64_str
+      getToken(imageFile);
+   },false);
+
+    // textarea drag event
+    // supply drag local files upload only
+    createform.addEventListener('dragstart',function (e){
+      e = e || window.event;
+
+      e.preventDefault();
+    },false);
+
+    createform.addEventListener('dragenter',function (e){
+      e = e || window.event;
+
+      e.preventDefault();
+    },false);
+
+    createform.addEventListener('dragover',function (e){
+      e = e || window.event;
+
+      e.preventDefault();
+    },false);
+
+    createform.addEventListener('drop',function (e){
+      e = e || window.event;
+
+      e.preventDefault();
+      // get filelist
+      let fileList = e.dataTransfer.files;
+      console.log(fileList);
+      if(fileList.length === 0 || fileList[0].type.indexOf('image') === -1){
+        let tips_drag = '请拖入正确数量本地图片文件';
+
+        navigator_off(tips_drag);
+        return;
+      }
+
+      getToken(fileList[0]);
+    },false);
+   })();
   }
 });
